282f0b7800eea64494dd203d6500d64826626560
acs3ss <acs3ss@virginia.edu>
2017-08-09 15:29:21 +1200

    Changing colors

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 window.onload = () => {
-	const text = ["Insert inspirational quotes here"];
+	const text = ["Insert inspirational quotes here", "So inspirational", "That's terrific"];
 	const x = Math.floor(Math.random() * text.length);
 	$('#randomQuote').html(text[x]);
 };
+
 
