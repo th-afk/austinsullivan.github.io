133440b9dfeac900be2d51e15164e1db9303b5fe
Austin Sullivan <acs3ss@virginia.edu>
2017-10-02 10:01:44 +1300

    Until I learn JS...

--- scripts.js
+++ scripts.js
@@ -1,8 +1,2 @@
-window.onload = () => {
-	const text = ["Awesome quote here",
-	              'Such great quotes here'];
-	const x = Math.floor(Math.random() * text.length);
-	$('#randomQuote').html(text[x]);
-};
 
 
